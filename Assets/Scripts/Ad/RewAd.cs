e64328b69dfc4062dd4176ecf3b52bb29c7b0619
logalexus <aliosha.log@yandex.ru>
2021-11-13 17:23:50 +0500

    Начал интегрировать рекламу

---
+++ Assets/Scripts/Ad/RewAd.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using GoogleMobileAds.Api;
+
+public class RewAd : MonoBehaviour
+{
+    private RewardedAd _rewardedAd;
+    private string _rewardedUnitId = "ca-app-pub-3940256099942544/5224354917";
+
+    private void OnEnable()
+    {
+        _rewardedAd = new RewardedAd(_rewardedUnitId);
+        AdRequest adRequest = new AdRequest.Builder().Build();
+        _rewardedAd.LoadAd(adRequest);
+        _rewardedAd.OnUserEarnedReward += OnUserEarnedReward;
+    }
+
+    private void OnUserEarnedReward(object sender, Reward e)
+    {
+        Player.Instance.CollectedCoinsInGame *= 2;
+    }
+
+    public void ShowAd()
+    {
+        if (_rewardedAd.IsLoaded())
+            _rewardedAd.Show();
+    }
+}
