d38ae2543de1b0b8855f6791771a45fffa3c48ec
logalex <logalex2001@yandex.ru>
2021-12-03 12:55:31 +0500

    Добавил новую карту

--- Assets/Scripts/Ad/InterAd.cs
+++ Assets/Scripts/Ad/InterAd.cs
@@ -7,6 +7,7 @@ public class InterAd : MonoBehaviour
 {
     private InterstitialAd _interstitialAd;
     private string _interstitialUnitId = "ca-app-pub-3940256099942544/1033173712";
+    //private string _interstitialUnitId = "ca-app-pub-2866108726683711/8067088553";
 
     private void OnEnable()
     {
